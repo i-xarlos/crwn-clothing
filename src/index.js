7990d92b470b38d9dfce209fe9a16d94b332f581
Carlos <ixarlos@gmail.com>
2020-05-12 19:53:27 -0500

    redux persistor add to our app

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root'),
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistconfig = {
+	key: 'root',
+	storage,
+	whitelist: ['cart'], // because user is storage outsitde
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 });
+
+export default persistReducer(persistconfig, rootReducer);
