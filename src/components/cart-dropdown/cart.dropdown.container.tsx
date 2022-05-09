64ab9c566ddfdac6124d333ba611f113036c826d
Carlos Gil <39429604+i-xarlos@users.noreply.github.com>
2022-05-09 10:52:09 -0400

    Optimization (#47)
    * OPTIMIZATION Added Suspense and lazy to router

---
+++ src/components/cart-dropdown/cart.dropdown.container.tsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { createStructuredSelector } from 'reselect'
+
+import { selectHasCartItems } from '../../store/cart/cart.selectors'
+import CartDropdown from './cart-dropdown.component'
+import WidthSpinner from '../../components/spinner/spinner.component'
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectHasCartItems,
+})
+
+const CartDropdownContainer = compose(
+  connect(mapStateToProps),
+  WidthSpinner
+)(CartDropdown)
+
+export default CartDropdownContainer
